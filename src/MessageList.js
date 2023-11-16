b4d664e3dc5ebb801fefc8362af654c453f70132
fowobi <109865505+fowobi@users.noreply.github.com>
2023-11-16 11:13:17 +0000

    Update MessageList.js

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,25 +1,3 @@
-// import React from "react";
-
-// const MessageList = ({ messages, onDelete }) => {
-//   return (
-//     <div>
-//       <h2>Chat Messages</h2>
-//       <ul>
-//         {messages.map((message) => (
-//           <li key={message.id}>
-//             <strong>{message.from}: </strong>
-//             {message.text}
-//             <button onClick={() => onDelete(message.id)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default MessageList;
-
-
 import React from "react";
 
 const MessageList = ({ messages, onDelete }) => {
