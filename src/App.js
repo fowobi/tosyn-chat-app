d9824a5ef0f11fe2881754d190a9904d442fc275
fowobi <109865505+fowobi@users.noreply.github.com>
2024-06-13 20:38:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import MessageForm from "./MessageForm";
 import "./styles.css";
 import config from "./config";
 
-
 const apiUrl = process.env.REACT_APP_API_URL || config.apiUrl;
 
 const App = () => {
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true); // State to manage loading indicator
 
   useEffect(() => {
     fetch(`${apiUrl}/messages`)
       .then((response) => response.json())
-      .then((data) => setMessages(data))
-      .catch((error) => console.error("Error fetching messages:", error));
+      .then((data) => {
+        setMessages(data);
+        setLoading(false); // Set loading to false once data is fetched
+      })
+      .catch((error) => {
+        console.error("Error fetching messages:", error);
+        setLoading(false); // Set loading to false in case of error
+      });
   }, []);
 
   const handleDelete = (id) => {
@@ -35,7 +41,6 @@ const App = () => {
       .catch((error) => console.error("Error deleting message:", error));
   };
 
-
   const handleMessageSubmit = (newMessage) => {
     fetch(`${apiUrl}/messages`, {
       method: "POST",
@@ -53,7 +58,11 @@ const App = () => {
     <div className="chat-name">
       <h1>Tosin's CYF Chat</h1>
       <MessageForm onMessageSubmit={handleMessageSubmit} />
-      <MessageList messages={messages} onDelete={handleDelete} />
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <MessageList messages={messages} onDelete={handleDelete} />
+      )}
     </div>
   );
 };
