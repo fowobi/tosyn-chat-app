925362597592f05b9588095f3f25c667bb07cda4
fowobi <109865505+fowobi@users.noreply.github.com>
2025-07-05 18:29:24 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
-const config = {
-    apiUrl: "https://tosin-chat-server1.glitch.me", 
-  };
+// const config = {
+//     apiUrl: "https://tosin-chat-server1.glitch.me", 
+//   };
   
-  export default config;
+//   export default config;
   
+const isLocalhost = window.location.hostname === "localhost";
+
+const apiUrl = isLocalhost
+  ? "http://localhost:18080"
+  : "https://tosin-chat-server1.glitch.me";
+
+const config = { apiUrl };
+
+export default config;
